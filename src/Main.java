8260473ba3276fd0397b02a5adb978fd344c7274
parisa75 <p.hoseyni75@gmail.com>
2021-01-24 10:56:49 -0800

    push

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Driver Program
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter size of the array");
+        int arraySize = scan.nextInt();
+        int[] arr = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            System.out.println("Please enter numbers");
+            int input = scan.nextInt();
+            if (checkIsDuplicated(input, arr)) {
+                System.out.println("duplicated array item ,please enter again!");
+                i--;
+            } else arr[i] = input;
+        }
+        System.out.println("************* movement count is :" + Sorter.minMoves(arr, arr.length));
 
-        int arr[] = {20, 60, 40, 80};
-        int n = arr.length;
-        System.out.println(Sorter.minMoves(arr, n));
+
+     /*   int length = arr.length;
+        length = RemoveDuplicateInArrayExample3.removeDuplicateElements(arr, length);
+        //printing array elements
+        for (int i = 0; i < length; i++)
 
 
+            System.out.print(arr[i] + " ");
+        System.out.println(NumberSorting.minMoves(arr, length));*/
+    }
+
+    private static boolean checkIsDuplicated(int input, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == input) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,26 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
 
 public class Sorter {
-    static int minMoves(int[] arr, int n)
-    {
-        int end = arr.length;
-        Set<Integer> set = new HashSet<Integer>();
 
-        for(int i = 0; i < end; i++){
-            set.add(arr[i]);
-        }
-        Object[] array = set.toArray();
-
-
+    static int minMoves(int arr[], int n) {
         int expectedItem = n;
 
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if ((int)array[i] == expectedItem)
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == expectedItem)
                 expectedItem--;
         }
 
         return expectedItem;
     }
+
 }
